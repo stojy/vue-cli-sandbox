c5626ca12ef38be344e24e436564ed30757aa447
stoj <tibor@stoj.net>
2018-03-18 22:38:47 +0800

    added a dummy about component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>{{name}}</div>
+    <AboutDetail />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
+
+<script>
+import AboutDetail from "@/components/AboutDetail.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "about",
+  data() {
+    return {
+      name: "asdf"
+    };
+  },
+  components: {
+    AboutDetail
+  }
+};
+</script>
